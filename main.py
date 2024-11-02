9d6c12f6d158390bf261537d71558397951efcfd
epi4mack <rudenokmax1488@gmail.com>
2024-11-02 18:54:35 +0300

    fixed optimized 'iterate' function

--- main.py
+++ main.py
@@ -49,14 +49,16 @@ def is_happy(cell: tuple, grid) -> bool:
 def swap_unhappy_with_empty(grid, unhappy_cell: tuple, empty_cell: tuple) -> None:
     x1, y1 = unhappy_cell
     x2, y2 = empty_cell
-    grid[x1][y1], grid[x2][y2] = grid[x2][y2], grid[x1][y1]
+
+    # Прямой обмен значений
+    grid[x2][y2], grid[x1][y1] = grid[x1][y1], grid[x2][y2]
 
 
-def get_unhappy_cells(grid) -> list:
+def get_unhappy_cells(grid) -> set:
     return {cell for cell in np.ndindex(grid.shape) if not is_empty(cell, grid) and not is_happy(cell, grid)}
 
 
-def get_empty_cells(grid) -> list:
+def get_empty_cells(grid) -> set:
     return {cell for cell in np.ndindex(grid.shape) if is_empty(cell, grid)}
 
 
@@ -68,26 +70,70 @@ def update_happiness(cell: tuple, grid, unhappy_cells):
     else:
         unhappy_cells.add(cell)
 
+# Медленная функция
+# def iterate(grid, k: int) -> None:
+#
+#     for i in range(k):
+#         unhappy_cells = get_unhappy_cells(grid)
+#
+#         if not unhappy_cells:
+#             global total
+#             total = i
+#             return
+#
+#         empty_cells = get_empty_cells(grid)
+#
+#         random_empty = choice(list(empty_cells))
+#         random_unhappy = choice(list(unhappy_cells))
+#
+#         swap_unhappy_with_empty(grid, random_unhappy, random_empty)
 
-def iterate(grid) -> None:
 
-    while True:
-        unhappy_cells = get_unhappy_cells(grid)
+# Быстрая функция
+def iterate(grid, k: int) -> None:
+    unhappy_cells = get_unhappy_cells(grid)
+    empty_cells = get_empty_cells(grid)
+
+    for i in range(k):
 
         if not unhappy_cells:
+            global total
+            print(i, 'Закончили')
+            total = i
             return
 
-        empty_cells = get_empty_cells(grid)
-
-        random_empty = choice(list(empty_cells))
         random_unhappy = choice(list(unhappy_cells))
+        random_empty = choice(list(empty_cells))
 
-        swap_unhappy_with_empty(grid, random_empty, random_unhappy)
+        swap_unhappy_with_empty(grid, random_unhappy, random_empty)  # Теперь на месте random_empty находится random_unhappy
 
-        global total
-        total += 1
+        empty_cells.remove(random_empty)  # Теперь клетка на этих координатах точно НЕ пустая
+        empty_cells.add(random_unhappy)  # Клетка на координатах random_unhappy теперь точно пустая
 
-        
+        unhappy_cells.remove(random_unhappy)  # Теперь эта клетка пустая
+
+        # Теперь у нас есть новая НЕ ПУСТАЯ клетка, а также новая пустая клетка
+        new_cell, new_empty = random_empty, random_unhappy
+
+        if not is_happy(new_cell, grid):
+            unhappy_cells.add(new_cell)
+
+        for cell in get_neighbors(random_unhappy, grid):
+            if cell not in empty_cells:
+                if is_happy(cell, grid):
+                    unhappy_cells.discard(cell)
+                else:
+                    unhappy_cells.add(cell)
+
+        for cell in get_neighbors(new_cell, grid):
+            if cell not in empty_cells:
+                if is_happy(cell, grid):
+                    unhappy_cells.discard(cell)
+                else:
+                    unhappy_cells.add(cell)
+
+    total = k
+
 
 def unhappy_percentage(grid) -> float:
     total_cells = grid.size
@@ -103,8 +149,9 @@ if __name__ == '__main__':
 
     start = perf_counter()
 
-    n = 10
+    n = 100
     happiness_threshold = 2
+    k = 1_000_000
 
     total = 0
 
@@ -118,8 +165,7 @@ if __name__ == '__main__':
 
     final_grid = initial_grid.copy()
 
-    # Итерации (шаги) идут пока есть несчастные клетки
-    iterate(final_grid)
+    iterate(final_grid, k)
 
     unhappy = unhappy_percentage(final_grid)
     empty = empty_percentage(final_grid)
